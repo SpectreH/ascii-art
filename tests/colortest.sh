063c14d6f71152bdca28305f8cd4df03c1372ef8
Neeoo <denni.karin2803@gmail.com>
2021-09-27 22:15:45 +0300

    init ascii-art project

---
+++ tests/colortest.sh
@@ -0,0 +1,8 @@
+cd ..
+go run . "hello world" "standard" --color=red
+go run . "1 + 1 = 2" "standard" --color=green
+go run . "(%&) ??" "standard" --color=yellow [1-6]
+go run . "(%&) ??" "standard" --color=yellow [1]
+go run . "(%&) ??" "standard" --color=yellow [2-3]
+go run . "HeY GuYs" "standard" --color=orange [4-7]
+go run . "RGB()" "standard" --color=blue [2]
